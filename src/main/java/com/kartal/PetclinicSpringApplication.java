e4fbb510c30e915b14e08f46daa146aa33e392c5
Abdulah Kartal <abdullah.kartal@heybringo.com>
2023-08-09 11:25:58 +0300

    project files added

---
+++ src/main/java/com/kartal/PetclinicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kartal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kartal/PetclinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
